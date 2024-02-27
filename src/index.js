aafd653c28697c3391127f38e2b49ec211099bbf
Tin Vuksan <tin.vuksan@vuv.hr>
2024-02-27 17:47:08 +0100

    Init

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const { Client, IntentsBitField } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.GuildMessageReactions,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on("messageReactionAdd", async (reaction, user) => {
+  if (user.bot) return;
+  if (reaction.emoji.name === "andi") {
+    const andiJapaEmoji = reaction.message.guild.emojis.cache.find(
+      (emoji) => emoji.name === "andijapa"
+    );
+    reaction.message.reply(
+      `Judiii japa mi se ftaplje jebal sreču ${andiJapaEmoji}`
+    );
+  }
+  if (reaction.emoji.name === "sever_judges") {
+    const andiEmoji = reaction.message.guild.emojis.cache.find(
+      (emoji) => emoji.name === "andi"
+    );
+    const severEmoji = reaction.message.guild.emojis.cache.find(
+      (emoji) => emoji.name === "sever_judges"
+    );
+    reaction.message.reply(
+      `Judiii zapratite severa na tikToku brat MOI najačismo ${andiEmoji} ${severEmoji}`
+    );
+  }
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) return;
+  const lowerCaseMessage = message.content.toLowerCase();
+
+  if (lowerCaseMessage.includes("andi")) {
+    const andiEmoji = message.guild.emojis.cache.find(
+      (emoji) => emoji.name === "andi"
+    );
+    message.channel.send(`Pozdrav svima judiiii ${andiEmoji}`);
+  }
+});
+
+client.login(
+  "MTIxMjA2NTkzMDk3MzAyNDMwNg.Gm4XFn._XGI55mkmEqsXBdA1Ga8gTvKjaqWt3d8QKvwgI"
+);
